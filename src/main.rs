02720bf63db8e119e87a55c269828ee34f3de62f
josh rotenberg <joshrotenberg@gmail.com>
2024-03-30 09:17:43 -0700

    add a test to test coverage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
